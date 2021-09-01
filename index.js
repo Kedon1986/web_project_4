2b1f8601261fb892587cb06fd3e3521b03064cfa
Kedon Greenidge <kedon.greenidge@gmail.com>
2021-09-01 14:48:01 -0400

    Feature/ DOM Template
    Added JS for HTML Template element

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*header*/
-const profiletName = document.querySelector('.profile__name');
+const profilettitle = document.querySelector('.profile__title');
 const profileAboutMe = document.querySelector('.profile__about-me');
 
 /*buttons*/
@@ -9,14 +9,14 @@ let closed = document.querySelector(".popup__close");
 /*popup*/
 let popup = document.querySelector('.popup');
 let form = document.querySelector('.popup__form');
-let titleInput = document.querySelector('#popup-name');
+let titleInput = document.querySelector('#popup-title');
 let subtitleInput = document.querySelector('#popup-about');
 
 
 /*functions*/
 function openPopup() {
   popup.classList.toggle('popup_opened');
-  titleInput.value = profiletName.textContent;
+  titleInput.value = profilettitle.textContent;
   subtitleInput.value = profileAboutMe.textContent;
 }
 
@@ -28,7 +28,7 @@ function updateProfile(event) {
 
   event.preventDefault();
 
-  profiletName.textContent = titleInput.value;
+  profilettitle.textContent = titleInput.value;
   profileAboutMe.textContent = subtitleInput.value;
   
   closePopup();
@@ -38,5 +38,64 @@ function updateProfile(event) {
 form.addEventListener('submit', updateProfile, false);
 edit.addEventListener("click", openPopup); 
 closed.addEventListener("click", closePopup);
+
+
+      /*Card Template*/
+
+
+
+/*Card Array*/
+
+const initialCards = [
+  {
+    title: "Yosemite Valley",
+    src: "./images/yosemite-valley.png"
+  },
+  {
+    title: "Lake Louise",
+    src: "./images/lake-louise.png"
+  },
+  {
+    title: "Bald Mountains",
+    src: "./images/bald-mountains.png"
+  },
+  {
+    title: "Latemar",
+    src: "./images/latemar.png"
+  },
+  {
+    title: "Vanoise National Park",
+    src: "./images/vanoise-national-park.png"
+  },
+  {
+    title: "Lago di Braies",
+    src: "./images/lago-di-braies.png"
+  }
+]; 
+
+
+/*Card Template function */
+
+
+ for(i = 0; i < initialCards.length; i ++) {
+   const cardTemplate = document.querySelector('#card').content; // Template
+   const card = cardTemplate.querySelector('.card').cloneNode(true);
+   const cardContainer = document.querySelector('.elements'); // card container
+   const cardImage = card.querySelector('.card__image');
+   const cardTitle =  card.querySelector('.card__title');
+  
+   
+   cardImage.src = initialCards[i].src;// card image 
+   cardTitle.textContent = initialCards[i].title;// card title
+
+   cardContainer.append(card);
+ }
+
+ 
+  
+  
+
+
+
 
 
