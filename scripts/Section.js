4fc933a93e18b386434166b0e08141c0fd967a2e
Kedon Greenidge <kedon.greenidge@gmail.com>
2021-10-24 21:28:38 -0400

    Add new Classes

---
+++ scripts/Section.js
@@ -0,0 +1,14 @@
+class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._renderedItems = data;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+  renderer() {
+    this._renderedItems.forEach((item) => this._renderer(item));
+  }
+
+  addItem(element) {
+    this._container.append(element);
+  }
+}
